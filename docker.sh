042bb79d052b484a49fa14fe98360959e71b717d
Nicolas Kirchhoffer <42067072+nkirchhoffer@users.noreply.github.com>
2019-04-28 20:36:47 +0200

    fix de build docker

--- docker.sh
+++ docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} .
 docker push ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT}
 docker tag -f ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} ${DOCKER_REPOSITORY}
 docker push ${DOCKER_REPOSITORY}
