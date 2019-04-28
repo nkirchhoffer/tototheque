c249f56392f91be237c03ca553309563c0788cfa
Nicolas Kirchhoffer <42067072+nkirchhoffer@users.noreply.github.com>
2019-04-28 21:52:23 +0200

    l'erreur était externe à la pipeline

--- docker.sh
+++ docker.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-echo "$DOCKER_PASSWORD" > password
-docker login --username "$DOCKER_USERNAME" --password $(<password)
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 docker build -t ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} .
 docker push ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT}
-docker tag -f ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} ${DOCKER_REPOSITORY}
+docker tag ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} ${DOCKER_REPOSITORY}
 docker push ${DOCKER_REPOSITORY}
