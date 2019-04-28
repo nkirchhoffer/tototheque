feacf8781b3477f8a9aa2b6d9f567c5d436f326b
Nicolas Kirchhoffer <42067072+nkirchhoffer@users.noreply.github.com>
2019-04-28 21:42:38 +0200

    essai exotique

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password $(</dev/stdin)
 docker build -t ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} .
 docker push ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT}
 docker tag -f ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} ${DOCKER_REPOSITORY}
