d9ba0db07b8775843b6a91862c370e16f5a9cfda
Nicolas Kirchhoffer <42067072+nkirchhoffer@users.noreply.github.com>
2019-04-28 21:22:31 +0200

    fix password info

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker login -u "$DOCKER_USERNAME" --password-stdin <<< "$DOCKER_PASSWORD"
 docker build -t ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} .
 docker push ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT}
 docker tag -f ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} ${DOCKER_REPOSITORY}
